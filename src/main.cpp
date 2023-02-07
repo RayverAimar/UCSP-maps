422f4e8dd1d33759b213d7d32d443322760f3320
RayverAimar <rayver.munoz@ucsp.edu.pe>
2023-02-06 22:53:14 -0500

    Fix bug: project couldn't run because of method called in innapropiate way.

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-    Graph<Checkpoint, Passage> sketch;
-    Checkpoint* main_gate = new Checkpoint(1);
-    Checkpoint* second_gate = new Checkpoint(2);
+    Graph sketch;
+    Checkpoint* main_gate = new Checkpoint(1, "Newman");
+    Checkpoint* second_gate = new Checkpoint(2, "Duhamel");
     Passage* first_passage = new Passage(17.7, 1, 20.0);
     sketch.insert(main_gate);
     sketch.insert(second_gate);
